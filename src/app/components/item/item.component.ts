d0c6666e35636abcef4bc9edc1ea354a9cfe6fc6
Emmanuel Franc <emmanuel.franc.pro@gmail.com>
2017-01-09 21:59:39 +0100

    added button sell x10

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -15,14 +15,13 @@ export class ItemComponent {
   @Input() item: Consumable | Beer;
 
   constructor() {
-
   }
 
   isBuyable(item, multiplicator: number = 1): boolean {
     let buyable = true; // Item is true by default because ALL cost must be buyable
 
     item.forEach((cost)=> {
-      if((cost.qty * multiplicator) > cost.type.qty) {
+      if((cost.qty * multiplicator) >= cost.type.qty) {
         buyable = false; // If not buyable, set variable to false (see line 21)
       }
     });
@@ -31,24 +30,24 @@ export class ItemComponent {
   }
 
   buy(item, multiplicator: number = 1): void { // multiplicator default value = 1 (same as if statement)
-    if(this.isBuyable(item.cost)) {
+    if(this.isBuyable(item.cost, multiplicator)) {
       item.cost.forEach((cost) => {
         cost.type.qty -= (cost.qty * multiplicator);
       });
 
-      item.qty += 1;
+      item.qty += 1 * multiplicator;
     }
   }
 
-  sell(item): void {
-    if(item.qty > 0) {
+  sell(item, multiplicator: number = 1): void {
+    if(item.qty * multiplicator > 0) {
       // Remove one from the item
-      item.qty -= 1;
+      item.qty -= (1 * multiplicator);
 
       // Add X% of the item cost, to the stock
       item.cost.forEach((cost) => {
         if(cost.type.name == "money") {
-          cost.type.qty += (cost.qty / 2);
+          cost.type.qty += (cost.qty * multiplicator) / 2;
         }
       });
     }
