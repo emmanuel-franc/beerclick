a93c81f7ea8381acb7dc6f783a071c134dd690d2
Emmanuel Franc <efranc@sqli.com>
2017-01-15 15:01:12 +0100

    created buy

--- src/app/components/upgrade/upgrade.component.ts
+++ src/app/components/upgrade/upgrade.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Player} from "../../models/player.model";
 
+import * as _ from "lodash";
+
 @Component({
   selector: 'app-upgrade',
   templateUrl: './upgrade.component.html',
@@ -17,8 +19,34 @@ export class UpgradeComponent implements OnInit {
   ngOnInit() {
     console.log('player', this.player)
   }
-
-  test() {
   
+  isBuyable(item): boolean {
+    let buyable = true;
+    
+    item.forEach((price)=> {
+      if(price.qty > price.consumable.qty) {
+        buyable = false;
+      }
+    });
+    
+    return buyable;
+  }
+  
+  buyUpgrade(item) {
+    if(this.isBuyable(item.price)) {
+      //set unlock on this upgrade
+      let purchase = _.find(this.player.resources.upgrades, {'name': item.name});
+      purchase.purchased = true;
+      
+      if(item.category === "consumables") {
+        let unlock = _.find(this.player.resources.consumables, {'name': item.name});
+        unlock.unlocked = true;
+      }
+  
+      if(item.category === "beers") {
+        let unlock = _.find(this.player.resources.beers, {'name': item.name});
+        unlock.unlocked = true;
+      }
+    }
   }
 }
