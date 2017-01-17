1ecbd85d30753843271aced82a5c55618d850fd5
Emmanuel Franc <efranc@sqli.com>
2017-01-17 17:25:17 +0100

    review

--- src/app/components/perk/perk.component.ts
+++ src/app/components/perk/perk.component.ts
@@ -3,8 +3,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import {Player} from "../../models/player.model";
 import {Perk} from "../../models/perk.model";
 
-//import * as _ from "lodash";
-
 @Component({
   selector: 'app-perk',
   templateUrl: './perk.component.html',
@@ -24,6 +22,5 @@ export class PerkComponent implements OnInit {
 
   ngOnInit() {
     this.perks = this.player.resources.perks;
-    console.log('PERKSPERKS', this.perks)
   }
 }
