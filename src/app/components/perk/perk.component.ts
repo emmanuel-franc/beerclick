cab076ff2063d7cb82c6ef7a0192d3b129dccadb
Emmanuel Franc <efranc@sqli.com>
2017-01-17 15:35:18 +0100

    created component

---
+++ src/app/components/perk/perk.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-perk',
+  templateUrl: './perk.component.html',
+  styleUrls: ['./perk.component.scss']
+})
+export class PerkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
