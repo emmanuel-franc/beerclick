e86e6bc08813039649956489af26f740be12665c
Emmanuel FRANC <emmanuel.franc@sogelink.fr>
2017-01-30 13:23:18 +0100

    added spec.ts

---
+++ src/app/services/perk/perk.service.spec.ts
@@ -0,0 +1,16 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async, inject } from '@angular/core/testing';
+import { PerkService } from './event.service';
+
+describe('PerkService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [PerkService]
+    });
+  });
+
+  it('should ...', inject([PerkService], (service: PerkService) => {
+    expect(service).toBeTruthy();
+  }));
+});
