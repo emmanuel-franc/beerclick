d9216c175f8103aff52ced10e83eda931fdd711f
poney <arthur.agombart@gmail.com>
2017-06-27 10:14:21 +0200

    Fix requirements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ItemComponent } from './components/item/item.component';
@@ -29,9 +27,7 @@ import { PopinComponent } from './components/popin/popin.component';
     PopinComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserModule
   ],
   providers: [
     PlayerService,
